aaecbaac07b964a894053bb144549ff9e53232cd
Yusuf <ybkocaman@gmail.com>
2024-01-06 16:26:59 +0000

    Initial Commit

---
+++ MemoryAid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryAid
+//
+//  Created by Yusuf Burak on 06/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MemoryAid/MemoryAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryAidApp.swift
+//  MemoryAid
+//
+//  Created by Yusuf Burak on 06/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
